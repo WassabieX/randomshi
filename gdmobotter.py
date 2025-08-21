0ddf4975418df14f123c3895e56e1db39b593b1c
WassabieX <156346812+WassabieX@users.noreply.github.com>
2025-08-21 16:31:03 +0200

    Add files via upload

---
+++ gdmobotter.py
@@ -0,0 +1,28 @@
+from pynput.keyboard import Controller, Key
+import time
+
+keyboard = Controller()
+
+print("Starte in 5 Sekunden...")
+time.sleep(5)
+
+last_mana_time = time.time()
+
+try:
+    while True:
+        keyboard.press(Key.tab)
+        keyboard.release(Key.tab)
+        time.sleep(0.5)
+
+        keyboard.press('1')
+        keyboard.release('1')
+        time.sleep(2)
+
+        if time.time() - last_mana_time >= 100:
+            keyboard.press('7')
+            keyboard.release('7')
+            last_mana_time = time.time()
+except KeyboardInterrupt:
+    print("Gestoppt.")
+
+# bot von warsay dem boss1
